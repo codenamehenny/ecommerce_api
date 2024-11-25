5b659869f693dc021ee692604c28cd96d8ac7ed1
codenamehenny <genesis09m@hotmail.com>
2024-11-25 14:09:31 -0500

    added the 3 schemas to serialize and deserialize the models

---
+++ schemas.py
@@ -0,0 +1,26 @@
+# This file contains the schemas to serialize and deserialize the models
+
+from Marshmallow import Schema
+
+# User Schema
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        
+# Order Schema
+class OrderSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Order
+       
+# Product Schema
+class ProductSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Product
+
+# Initialize Schemas
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+order_schema = OrderSchema()
+orders_schema = OrdersSchema(many=True)
+product_schema = ProductsSchema()
+products_schema = ProductsSchema(many=True)
