031931c6688a1666637ffb7d8d72c2168421bbc3
codenamehenny <genesis09m@hotmail.com>
2024-11-26 20:08:57 -0500

    fixed all functions for order routes

--- routes/order_routes.py
+++ routes/order_routes.py
@@ -1,88 +1,96 @@
 from flask import Flask, request, jsonify
-from schemas import order_schema
-from models import Order
+from schemas import order_schema, orders_schema, product_schema, products_schema
+from models import Order, Product, User
 from app import db
 from marshmallow import ValidationError
 from models import User
 
-# creating an order
+# POST - creating an order
 @app.route('/orders', methods=['POST'])
 def create_order()
     try:
         order_data = order_schema.load(request.json)
     except ValidationError as e:
         return jsonify(e.messages), 400
-    
-    # if the proper information was input, it will create a user object    
-    new_order = Order(order_date = order_data['order_date'])
+    # check if user exits
+    user = db.session.get(User, order_data['user_id'])
+    if not user:
+        return jsonify({
+            "error": f"User with ID {order_data['user_id']} not found.
+            Please add the user."
+            }), 404
+    # if user is found and the required information was entered, it creates the order
+    user_id = order_data['user_id']
+    order_date = order_data['order_date']    
+    new_order = Order(user_id = user_id, order_date = order_date)
     db.session.add(new_order)
     db.session.commit()
     # sending user object back to confirm new user was added to the database
     return order_schema.jsonify(new_order), 201 
 
-#  Read all orders
-@app.route('/orders', methods=['GET'])
-def get_orders():
-    query = select(Order)
-    orders = db.session.execute(query).scalars().all()
-
+# GET - Read all orders for a user
+@app.route('/orders/user/<int:user_id>', methods=['GET'])
+def get_orders_for_user(user_id):
+    # checking if user exists and returning an error if not
+    user = db.session.get(User, order_data['user_id'])
+    if not user:
+        return jsonify({
+            "error": f"User with ID {order_data['user_id']} not found.
+            Please add the user."
+            }), 404
+    # getting all orders for a user
+    orders = db.session.get(Order, user_id).all()
     return orders_schema.jsonify(orders), 200
 
-# Read one order by ID
-@app.route('/orders/<int:id>', methods=['GET'])
-def get_order(id):
-    user = db.session.get(Order, id)
-    return order_schema.jsonify(order), 200
-
-# Update an order
-@app.route('/orders/<int:id>', methods=['PUT'])
-def update_order(id):
-    order = db.session.get(Order, id)
-    # Error if user not found
+# GET - Add a product to an order (preventing duplicates)
+@app.route('/orders/<int:order_id>/add_product/<int:product_id>', methods=['GET'])
+def add_product_to_order(order_id, product_id):
+    # checking if order exists
+    order = db.session.get(Order, order_id)
     if not order:
-        return jsonify({"message": "Invalid user id"}), 400
-    # If user found, new information will be updated
-    try:
-        order_data = order_schema.load(request.json)
-    except ValidationError as e:
-        return jsonify(e.messages), 400
-    
-    order.order_date = order_data['order_date']
-    # commiting chnages and return the user object back as confirmation of the update
+        return jsonify({
+            "error": f"Order ID {order_id} not found.
+            Please place a new order."
+            }), 404
+    # checking if product exists
+    product = db.session.get(Product, product_id)
+    if not product:
+        return jsonify({
+            "error": f"Product ID {order_id} not found.
+            Please add the product first."
+            }), 404
+    # preventing duplicates
+    if product in order.products:
+        return jsonify({
+            "error": f"Product ID {product_id} is
+            already in Order ID {order_id}"}), 400
+    # adding the product to the order and returning order object as confirmation
+    order.products.append(product)
     db.session.commit()
     return order_schema.jsonify(order), 200
 
-# Delete an order
+# GET - All products from an order
+@app.route('/orders/<int:order_id>/products', methods=['GET'])
+def get_products_for_order(order_id):
+    # returning error if order doesn't exist
+    order = db.session.get(Order, order_id)
+    if not order:
+        return jsonify({
+            "error": f"Order ID {order_id} not found."
+            }), 404
+    # Get all products for an order
+    products = order.products
+    return products_schema.jsonify(products), 200    
+
+# DELETE - Delete an order
 @app.route('/orders/<int:id>', methods=['DELETE'])
-def delete_order(id):
-    order = db.session.get(Order, id)
+def delete_order(order_id):
+    order = db.session.get(Order, order_id)
     # returning an error if user not found
     if not order:
         return jsonify({"message": "Invalid order id"}), 400
+    
     # finding and deleting the user, then committing the change, also returning confirmation
     db.session.delete(order)
     db.session.commit()
-    return jsonify({"message": f"succefully deleted order {id}"}), 200
-
-# Associate a single order with a user (one to one relationship)
-@app.route('/users/<int:user_id>/add_order/<int:order_id>', methods=['GET'] )
-def user_order(user_id, order_id):
-    user = db.session.get(User, user_id)
-    order = db.session.get(Order, order_id)
-
-    user.pets.append(order)
-    db.session.commit()
-    return jsonify({"message": f"{user.name} placed order #{order.id} on {order.order_date}!"}), 200
-
-# Associate one user with multiple orders (one to many relationship)
-@app.route('/users/<int:user_id>/add_orders', methods=['POST'])
-def add_orders(user_id):
-    user = db.session.get(User, user_id)
-    order_data = request.json
-
-    for id in order_data['order_ids']:
-        order = db.session.get(Order, id)
-        user.orders.append(order)
-        db.session.commit()
-
-    return jsonify({"message": "All orders added!"}), 200
+    return jsonify({"message": f"succefully deleted order {order_id}"}), 200
