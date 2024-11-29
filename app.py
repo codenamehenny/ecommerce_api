07120a80af9543d3d3b9cce240aab8b333f52192
codenamehenny <genesis09m@hotmail.com>
2024-11-29 11:59:10 -0500

    registered blueprints for user, order and product

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os
 from models import Base
+from user_routes import user_blueprint
+from order_routes import order_blueprint
+from product_routes import product_blueprint
 
 # initiatializing Flask app
 app = Flask(__name__)
@@ -20,3 +22,8 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+# Registering blueprints
+app.register_blueprint(user_blueprint, url_prefix='/api')
+app.register_blueprint(order_blueprint, url_prefix='/api')
+app.register_blueprint(product_blueprint, url_prefix='/api')
